7e7fc69225f2d1a1904299952aa060b409692b7c
satpil <59343220+satpil@users.noreply.github.com>
2021-02-22 00:21:17 +0530

    interface

--- src/index.ts
+++ src/index.ts
@@ -70,27 +70,103 @@ let object: { name: string; age: number } = {
 object = { name: "sa", age: 25 };
 
 //type
-type Complex = {data: number[],output:(all:boolean)=>number[]}
+type Complex = { data: number[]; output: (all: boolean) => number[] };
 const complex2: Complex = {
-  data: [12,45,455],
-  output: function(all:boolean):number[]{
-    return this.data
+  data: [12, 45, 455],
+  output: function (all: boolean): number[] {
+    return this.data;
   }
-}
+};
 
 //union type
 let realage: number | string = 27;
-realage=25
+realage = 25;
 
 //chexk type
 let checkType = 255;
 
-if(typeof checkType === 'number'){
-  console.log('test')
+if (typeof checkType === "number") {
+  console.log("test");
 }
 
 //never
-function( never():never{
-  throw new Error('new error');
-  
+function never(): never {
+  throw new Error("new error");
 }
+
+//exercise
+type bankaccount = { money: number; deposit: (value: number) => number };
+type myself2 = { name: string; bankaccount: bankaccount; hobbies: string[] };
+let bankoccount: bankaccount = {
+  money: 1245,
+  deposit(value: number): number {
+    return (this.money += value);
+  }
+};
+
+let myself: myself2 = {
+  name: "allsl",
+  bankaccount: bankoccount,
+  hobbies: ["sport", "cooking"]
+};
+myself.bankaccount.deposit(3000);
+console.log(myself);
+
+//class
+// class Person {
+//  name: string;
+//  protected age: number = 25;
+//  private type: string;
+
+//  constructor(name:string,public username: string){
+//    this.name = name
+//  }
+// }
+
+// const person = new Person('sa','Sa')
+// console.log(person)
+
+//namespace
+
+namespace Person {
+  export let callPerson = (name: string): string => {
+    return name;
+  };
+}
+const name = "nama";
+console.log(Person.callPerson("satish"));
+console.log(name);
+
+//interface
+
+interface myName {
+  name: string;
+  age?: number;
+  [propName: string]: any;
+}
+
+function Person(person: myName) {
+  return person.name;
+}
+
+const PersonObject = {
+  name: "satish",
+  age: 25,
+  hobbies: 25
+};
+
+console.log(Person({ name: "sa", hobbies: 25 }));
+
+//generic
+
+function generic<T>(data: T) {
+  return data;
+}
+console.log(generic("sarinf").length);
+console.log(generic<number>(45));
+console.log(generic({ name: "sa", age: 45 }));
+
+//build in generic
+let arrray: Array<number> = [15, 56];
+arrray.push(56);
+arrray.push("sas");
